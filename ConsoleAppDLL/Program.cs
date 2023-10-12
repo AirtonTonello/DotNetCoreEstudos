0c67787ce9aac64d8599c4a5e4e33e81f79bbfa5
Airton Luiz Tonello Junior <airtonluizjunior@hotmail.com>
2023-10-12 14:12:41 -0300

    Compartilhamento de Pacotes

---
+++ ConsoleAppDLL/Program.cs
@@ -0,0 +1,20 @@
+﻿using LibSharedDLL;
+
+if (Utility.TestDLL())
+{
+    //for (int i = 0; i < 10; i++)
+    //{
+    //    Console.WriteLine($"Guid DLL: {Utility.TestGUID()}");
+    //}
+
+    foreach (var guid in Utility.TestGUIDList(10))
+    {
+        Console.WriteLine($"Guid DLL: {guid}");
+    }
+}
+else
+{
+    Console.WriteLine("Não foi");
+}
+
+Console.ReadKey();
---
+++ ConsoleAppNuget/Program.cs
@@ -0,0 +1,15 @@
+﻿using LibSharedDLL;
+
+if (Utility.TestDLL())
+{
+    foreach (var guid in Utility.TestGUIDList(10))
+    {
+        Console.WriteLine($"Guid DLL: {guid}");
+    }
+}
+else
+{
+    Console.WriteLine("Não foi");
+}
+
+Console.ReadKey();
